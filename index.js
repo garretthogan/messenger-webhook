338a535c69b49744367de5c82b861940fe15e93e
Garrett Hogan <garrett.hogan.dev@gmail.com>
2019-08-25 18:38:59 -0700

    handling postback options

--- index.js
+++ index.js
@@ -2,6 +2,103 @@
 
 require('dotenv').config();
 
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageText = message.text;
+
+  if (messageText) {
+    switch (messageText) {
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  }
+}
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  switch (payload) {
+    case 'get_started':
+      sendGetStarted(senderID);
+      break;
+    case 'studio_time':
+      sendTextMessage(senderID, "Reserve Studio Time");
+      break;
+    case 'cancel_reservation':
+      sendTextMessage(senderID, "Cancel Reservation");
+      break;
+    default:
+      sendTextMessage(senderID, "Postback called");
+  }
+}
+
+function sendGetStarted(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: "How can I help you today?",
+          buttons: [{
+            type: "postback",
+            title: "Reserve Studio Time",
+            payload: "studio_time"
+          }, {
+            type: "postback",
+            title: "Cancel Reservation",
+            payload: "cancel_reservation"
+          }]
+        }
+      }
+    }
+  };
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  console.log(messageData);
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s",
+          messageId, recipientId);
+      } else {
+        console.log("Successfully called Send API for recipient %s",
+          recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });
+}
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -12,30 +109,32 @@ app.get('/', (req, res) => {
   res.status(200).send('All good');
 })
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', function (req, res) {
+  var data = req.body;
 
-  const body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      const webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function (pageEntry) {
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function (messagingEvent) {
+        if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
     });
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
   }
-
 });
 
 // Adds support for GET requests to our webhook
